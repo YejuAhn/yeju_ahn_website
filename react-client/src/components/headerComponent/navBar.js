44b4a21af882a07201db095c9de1c561f0ed426a
Yeju Ahn <38146746+YejuAhn@users.noreply.github.com>
2020-01-12 08:57:09 -0500

    Add files via upload

--- react-client/src/components/headerComponent/navBar.js
+++ react-client/src/components/headerComponent/navBar.js
@@ -1,16 +1,39 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Menu, Segment, Header, Button } from 'semantic-ui-react'
 
 class NavBar extends Component {
-  render() {
-    return (
-      <header>
-        <ul id="headerButtons">
-          <li className="navButton"><Link to="">Home</Link></li>
-        </ul>
-      </header>
-    )
-  }
+    state = { activeItem: 'About' }
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+    render() {
+        const { activeItem } = this.state
+        return (
+            <Menu borderless>
+                <Menu.Item header>ESTHER YEJU AHN</Menu.Item>
+                 <Menu.Menu position='right'>
+                    <Menu.Item
+                      name='About'
+                      active={activeItem === 'About'}
+                      onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                      name='Portfolio'
+                      active={activeItem === 'Portfolio'}
+                      onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                      name='Writing'
+                      active={activeItem === 'Writing'}
+                      onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                      name='Contact'
+                      active={activeItem === 'Contact'}
+                      onClick={this.handleItemClick}
+                    />
+                </Menu.Menu>             
+            </Menu> 
+        )
+    }
 }
 
 export default NavBar;
