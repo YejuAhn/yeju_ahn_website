950142a1a562eac177a497f93b3404d02091b6bc
Yeju Ahn <38146746+YejuAhn@users.noreply.github.com>
2020-01-13 20:58:10 -0500

    homepage updated

--- react-client/src/components/pages/homePage.js
+++ react-client/src/components/pages/homePage.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Header, Icon, Image, Segment} from 'semantic-ui-react'
+import Logo from "./IMG_8752.JPG"
 
 class HomePage extends Component {
   render() {
     return (
-      <div>
-        Home page
-      </div>
+        <div>
+            <Header as='h1' icon textAlign='center'>
+                <Image src={Logo} size='massive' centered />
+                <Header.Content>Esther Yeju Ahn</Header.Content>
+            </Header>
+        </div>
     )
   }
 }
