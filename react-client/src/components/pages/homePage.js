f567ad49f7ab10ecebf377a02c5923c767003b37
Yeju Ahn <yejuahn@Yejus-MacBook-Air.local>
2020-07-26 15:52:27 +0900

    Reinstall dev dependencies, start designing back again

--- react-client/src/components/pages/homePage.js
+++ react-client/src/components/pages/homePage.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import { Header, Icon, Image, Segment} from 'semantic-ui-react'
-import Logo from "./IMG_8752.JPG"
+import image from "../assets/images/yeju_illustration.jpg"
 
 class HomePage extends Component {
   render() {
     return (
         <div>
             <Header as='h1' icon textAlign='center'>
-                <Image src={Logo} size='massive' centered />
+                <Image src={image} size='massive' centered />
                 <Header.Content>Esther Yeju Ahn</Header.Content>
             </Header>
         </div>
