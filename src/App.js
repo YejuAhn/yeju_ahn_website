cc181ed3dffc1e3b57065a71ba892e48ba01cf00
Yeju Ahn <yejuahn@Yejus-MacBook-Air.local>
2020-09-03 01:00:11 +0900

    Add background image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component, createRef} from 'react';
 import './App.css';
 import Portfolio from './component/Home/Portfolio';
 import Contact from './component/Home/Contact';
 import Home from './component/Home/Home';
 import Header from './component/Header/Header';
 import 'semantic-ui-css/semantic.min.css';
+import background from "./asset/background.jpg";
+
 
 class App extends Component {
-  render() {
+    contextRef = createRef();
+    render() {
     return (
-        <div>
+        <div className="container">
             <Header> </Header>
             <Home> </Home>
             <Contact> </Contact>
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,6 +1,6 @@
-import React, {Component} from "react";
+import React, {Component, createRef} from "react";
 import {StyledMenu} from "./StyledHeader";
-import {Container, Icon, Image, Menu, Dropdown} from 'semantic-ui-react';
+import {Container, Icon, Sticky, Menu, Dropdown} from 'semantic-ui-react';
 import './Header.css';
 import HeaderContact from './HeaderContact';
 
@@ -10,38 +10,47 @@ class Header extends Component {
     render() {
         const { activeItem } = this.state;
         return (
-            <StyledMenu size = "large">
-                <Icon name='paw' size='huge' style = {{paddingRight: '10px'}}/>
-                <br/>
-                <Menu.Item
-                    name = "home"
-                    active={activeItem === 'home'}
-                    onClick={this.handleItemClick}
+            <div ref={this.contextRef}>
+                <Sticky context = {this.contextRef}>
+                <StyledMenu
+                    size = "large"
+                    style = {{fontSize: '15px',
+                    backgroundColor : '#'}}
+                    attached='top'
+                    tabular>
+                    <Icon name='paw' size='large' style = {{paddingRight: '20px', paddingTop: '10px'}}/>
+                    <br/>
+                    <Menu.Item
+                        name = "home"
+                        active={activeItem === 'home'}
+                        onClick={this.handleItemClick}
+                        />
+                    <Menu.Item
+                        name='Portfolio'
+                        active={activeItem === 'Portfolio'}
+                        onClick={this.handleItemClick}
                     />
-                <Menu.Item
-                    name='Portfolio'
-                    active={activeItem === 'Portfolio'}
-                    onClick={this.handleItemClick}
-                />
-                <Menu.Item
-                    name='Resume'
-                    active={activeItem === 'Resume'}
-                    onClick={this.handleItemClick}
-                />
-                <Menu.Item
-                    name='Blog'
-                    active={activeItem === 'Blog'}
-                    onClick={this.handleItemClick}
-                />
-                <StyledMenu.Menu position='right'>
-                    <Dropdown item text='Language'>
-                        <Dropdown.Menu>
-                            <Dropdown.Item>English</Dropdown.Item>
-                            <Dropdown.Item>Korean</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </StyledMenu.Menu>
-            </StyledMenu>
+                    <Menu.Item
+                        name='Resume'
+                        active={activeItem === 'Resume'}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name='Blog'
+                        active={activeItem === 'Blog'}
+                        onClick={this.handleItemClick}
+                    />
+                    <StyledMenu.Menu position='right'>
+                        <Dropdown item text='Language'>
+                            <Dropdown.Menu>
+                                <Dropdown.Item>English</Dropdown.Item>
+                                <Dropdown.Item>Korean</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </StyledMenu.Menu>
+                </StyledMenu>
+            </Sticky>
+        </div>
         )
     }
 }
