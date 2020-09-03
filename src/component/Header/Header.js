0c6e9888c12b8032a47b7099e939aeabbce458ab
Yeju Ahn <yejuahn@Yejus-MacBook-Air.local>
2020-09-03 09:52:13 +0900

    Add colors to navbar

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -2,12 +2,12 @@ import React, {Component, createRef} from "react";
 import {StyledMenu} from "./StyledHeader";
 import {Container, Icon, Sticky, Menu, Dropdown} from 'semantic-ui-react';
 import './Header.css';
-import HeaderContact from './HeaderContact';
 
 class Header extends Component {
     state = { activeItem: 'home' };
     handleItemClick = (e, { name }) => this.setState({ activeItem: name });
     render() {
+        const { color } = this.props;
         const { activeItem } = this.state;
         return (
             <div ref={this.contextRef}>
@@ -15,8 +15,9 @@ class Header extends Component {
                 <StyledMenu
                     size = "large"
                     style = {{fontSize: '15px',
-                    backgroundColor : '#'}}
+                    backgroundColor: '#dcd0ff'}}
                     attached='top'
+                    color = {'purple'}
                     tabular>
                     <Icon name='paw' size='large' style = {{paddingRight: '20px', paddingTop: '10px'}}/>
                     <br/>
