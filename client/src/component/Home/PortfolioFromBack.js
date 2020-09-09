8a0313d828fdd339a4c28e72910d7a86a75b24a3
Yeju Ahn <yejuahn@Yejus-MacBook-Air.local>
2020-09-10 07:16:59 +0900

    Add html5 design template

--- client/src/component/Home/PortfolioFromBack.js
+++ client/src/component/Home/PortfolioFromBack.js
@@ -1,16 +1,35 @@
 import React, {Component} from "react";
-import {Image, Grid, Label, Modal} from "semantic-ui-react";
+import {Image, Grid, Label, Modal, Button, Icon} from "semantic-ui-react";
 import './Portfolio.css';
+
 
 class PortfolioFromBack extends Component {
     constructor(props) {
         super(props);
         this.state = {
             projects: [],
-            modalOpen: false,
-        };
+            showModal: false,
+            modalId: null,
+            data:{}
+        }
+    }
+    showModal = (e, index) => {
+        this.setState({ showModal: true, modalId: index });
+        console.log("Index: " + index);
+    };
+
+    hideModal = (e, index) => {
+        this.setState({ showModal: false, modalId: index });
+        console.log("Index: " + index);
+    };
+
+    disp(e) {
+        console.log("function: " + e._id);
     }
 
+    showArticle(e) {}
+
+    modalClose = () => this.setState({ showModal: false }); //change
     async componentDidMount() {
         try {
             const res = await fetch('http://127.0.0.1:8000/api/'); // fetching the data from api, before the page loaded
@@ -31,22 +50,23 @@ class PortfolioFromBack extends Component {
                         <Grid.Column>
                             <div key = {item.id} className = 'projectItem'>
                                 <Label className = 'projectTitle' attached='bottom left'>{item.project_title}</Label>
-                                <Modal
-                                    open = {this.state.open}
-                                    onClose = {() => this.setState({modalOpen : false})}
-                                    onOpen =  {() => this.setState({modalOpen : true})}
-                                    trigger = {<Image src = {item.image} size='large' rounded/>}
-                                >
-                                    <Modal.Content image scrolling>
-                                        <Image src = {item.image} size='large' rounded/>
-                                        <Modal.Description>
+                                    <Modal.Content image scrolling
+                                       show={this.state.showModal}
+                                       onHide={this.modalClose}
+                                       trigger = {<Image key = {item.id} src = {item.image} size='large'/>}>
+                                        <Modal.Description key = {item.id} >
+                                            <Image src = {item.image} size='large' />
                                             <p>
-                                            This is an example of expanded content that will cause the modal's
-                                            dimmer to scroll.
+                                                This is an example of expanded content that will cause the modal's
+                                                dimmer to scroll.
                                             </p>
                                         </Modal.Description>
                                     </Modal.Content>
-                                </Modal>
+                                    <Modal.Actions>
+                                        <Button onClick={this.hideModal} primary>
+                                            <Icon name='chevron right' />
+                                        </Button>
+                                    </Modal.Actions>
                             </div>
                         </Grid.Column>
                     ))}
